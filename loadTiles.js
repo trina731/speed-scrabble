c49548e8c10363ea538e0797dcf15e27a80c5392
Trina Chatterjee <trinac0731@gmail.com>
2020-03-28 11:37:48 -0700

    working on flip

---
+++ loadTiles.js
@@ -0,0 +1,20 @@
+//Read from file which 
+$.get( "letters.txt", function( data ) {
+    let txt = data.toString().split("\n");
+
+    //First line has the number of letters in the alphabet
+    let alphSize = Number(txt[0]);
+
+    //Initialize from file
+    for(i = 0; i < alphSize; i++){
+        alph += txt[i+1].substring(0,1);
+        unflipped[i] = Number(txt[i+1].substring(1));
+        numBag += unflipped[i];
+        for(j = 0; j < unflipped[i]; j++){
+            allUnflippedTiles += alph.substring(i);
+        }
+    }   
+    
+    flipped = Array(numBag).fill(0);
+    allTiles = Array(numBag).fill("_"); 
+});
