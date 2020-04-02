28269653dd692e31ff626651d5c6945e34b21209
MrZmann <reed.z@utexas.edu>
2020-04-02 00:40:08 -0500

    Added textbox + displays multiplayer info, debug capability

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var DEBUG = 1;
+
 var express = require('express');
 var app = express();
 var serv = require('http').Server(app);
@@ -13,27 +15,67 @@ serv.listen(2000);
 console.log("Server started");
   
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+ 
+var Player = function(id){
+    var self = {
+        words:[],
+        score:0,
+        id:id,
+        number:"" + Math.floor(100 * Math.random()),
+    }
+    // left this in just to show an example of a function
+    // self.updatePosition = function(){
+    //     if(self.pressingRight)
+    //         self.x += self.maxSpd;
+    //     if(self.pressingLeft)
+    //         self.x -= self.maxSpd;
+    //     if(self.pressingUp)
+    //         self.y -= self.maxSpd;
+    //     if(self.pressingDown)
+    //         self.y += self.maxSpd;
+    // }
+    return self;
+}
 
 var io = require("socket.io")(serv,{});
 io.sockets.on('connection', function(socket){
     socket.id = Math.random();
-    socket.x = 0;
-    socket.y = 0;
-    socket.number = "" + Math.floor(10 * Math.random());
     SOCKET_LIST[socket.id] = socket;
+ 
+    var player = Player(socket.id);
+    PLAYER_LIST[socket.id] = player;
+   
+    socket.on('disconnect',function(){
+        delete SOCKET_LIST[socket.id];
+        delete PLAYER_LIST[socket.id];
+    });
 
     socket.on('disconnect', function(){
         delete SOCKET_LIST[socket.id];
     });
     console.log("socket connection");
 
-    socket.on('happy', function(data){
-        console.log('happy because: ' + data.reason);
+    socket.on('submitWord', function(data){
+        //Just testing out the next couple lines
+        PLAYER_LIST[socket.id].words.push(data);
+        PLAYER_LIST[socket.id].score += data.length;
+        for(s in SOCKET_LIST){
+            SOCKET_LIST[s].emit("updateWordDisplay", JSON.stringify(PLAYER_LIST));
+        }
+        //TODO:
+        //Check if in dictionary
+        //Check if playable
+        //Update display and pts if necessary
     });
 
-    socket.emit('serverMsg', {
-        msg:'hello',
-    });
+    socket.on('evalServer', function(data){
+        //Running eval(data) is dangerous, so don't let real users do it
+        if(!DEBUG)
+            return;
+        var res = eval(data);
+        socket.emit('evalAnswer', res);
+    })
 });
 
 setInterval(function(){
